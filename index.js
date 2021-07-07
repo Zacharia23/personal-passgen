cae4b54c4850da0eb0683d6e5a736a2dca4763f6
Zacharia23 <zachariabaraka8@gmail.com>
2021-07-07 14:22:08 +0300

    Commander Options Setup

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-if(process.argv[2] === 'generate') {
-    console.log('Generated')
-}
+const program = require('commander')
+
+program.version('1.0.0').description('Simple Password Generator')
+
+
+program
+    .option('-l, --length <number>', 'length of password', '8')
+    .option('-s, --save', 'Save password to passwords.txt')
+    .option('-nn, --no-numbers', 'Remove Numbers')
+    .option('-ns, --no-symbols', 'Remove Symbols')
+    .parse()
+
+const { length, save, numbers, symbols} = program.opts()
+
+console.log(length, symbols)
